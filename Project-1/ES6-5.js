eb74cb524cdba3eb9fd50362d3247f596000adae
Aritra Das <aritradas080@gmail.com>
2023-08-20 14:00:32 +0600

    ok
    ok

---
+++ Project-1/ES6-5.js
@@ -0,0 +1,5 @@
+import {setText, text} from './myModule.js'
+
+console.log(text);
+setText("This is js file");
+console.log(text);
---
+++ Project-1/myModule.js
@@ -0,0 +1,5 @@
+export let text = "hello this is a module";
+
+export function setText(txt){
+    text = txt;
+}
